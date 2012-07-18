e3081aed64ea8a0b4567f5cb108c9c7acdea8139
Celia Pansard <celia.pansard@kitware.com>
2012-07-18 14:01:03 -0400

    Community list works

--- App-example/gen/com/app_example/R.java
+++ App-example/gen/com/app_example/R.java
@@ -20,24 +20,31 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int edit_message=0x7f080000;
-        public static final int menu_settings=0x7f080001;
+        public static final int mainListView=0x7f080000;
+        public static final int menu_settings=0x7f080004;
+        public static final int product_label=0x7f080003;
+        public static final int rowTextView=0x7f080002;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_list_of_views=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_simplerow=0x7f030002;
+        public static final int single_list_item_view=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
     }
     public static final class string {
+        public static final int Instruction1=0x7f050008;
         public static final int app_name=0x7f050000;
         public static final int button_access_web=0x7f050007;
-        public static final int button_send=0x7f050002;
-        public static final int edit_message=0x7f050001;
-        public static final int menu_settings=0x7f050003;
-        public static final int title_activity_display_message=0x7f050005;
-        public static final int title_activity_http_request=0x7f050006;
-        public static final int title_activity_main=0x7f050004;
+        public static final int menu_settings=0x7f050001;
+        public static final int title_activity_display_message=0x7f050003;
+        public static final int title_activity_http_request=0x7f050004;
+        public static final int title_activity_list_of_views=0x7f050005;
+        public static final int title_activity_main=0x7f050002;
+        public static final int title_activity_single_list_item=0x7f050006;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
--- App-example/src/com/app_example/MainActivity.java
+++ App-example/src/com/app_example/MainActivity.java
@@ -1,108 +1,108 @@
 package com.app_example;
 
-//import android.R;
+//----------------------------------------libraries----------------------------------------//
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
-//import android.view.MenuItem;
-//import android.support.v4.app.NavUtils;
-//import android.net.*;
 
-
-public class MainActivity extends Activity {
+//------------------------------------------------------------------------------------------//
+public class MainActivity extends Activity 
+{	
+	
+	//---------- Attributes-----------------------------------------------------------------//
+	public final static String EXTRA_MESSAGE = "com.example.myapp.MESSAGE";
 	
 	
-	public final static String EXTRA_MESSAGE = "com.example.myapp.MESSAGE";//?????
-	
-
+	//---------- ON CREATE-----------------------------------------------------------------//
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    //---------- ON CREATE OPTIONS MENU----------------------------------------------------//
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
     
-    //second activity
-    /** Called when the user selects the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-    	
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	//An Intent==object that provides runtime binding between separate components
-    	//used to start another activity called DisplayMessageActvity
-    	//the constructor-->two parameters:A Context(this) and The Class of the app component
-
-    	
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	//findViewById()==carry a bundle of data to the activity 
-    	
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	//putExtra()==takes a string as the key and the value in the second parameter.
-    	startActivity(intent);
-    }
-    public void accessWeb(View view) {
-    	
-// Do something in response to button
-    	//Intent intent = new Intent(this, HttpRequest.class);
-    	
-    	//An Intent==object that provides runtime binding between separate components
-    	//used to start another activity called DisplayMessageActvity
-    	//the constructor-->two parameters:A Context(this) and The Class of the app component
+  	//---------- FUNCTIONS FOR LIFECYCLE ACTIVITY------------------------------------------//
+    protected void onDestroy() {
+        super.onDestroy(); }
+      protected void onPause() {
+        super.onPause(); }
+      protected void onResume() {
+        super.onResume(); }
+      protected void onStart() {
+        super.onStart(); }
+      protected void onStop() {
+        super.onStop(); } 
+    
+  	//---------- ACCESS WEB-----------------------------------------------------------------//
+    public void accessWeb(View view) 
+    {	
+    	// Do something in response to button
     	this.get("http://midas3.kitware.com/midas/api/json?method=midas.community.list");
-    	
-    	
-    	
     }
-    public void get(String sUrl) {
+    
+  	//---------- GET ----------------------------------------------------------------------//
+    public void get(String sUrl) 
+    {
 		HttpThread thread = new HttpThread(this, sUrl);
 		Thread t = new Thread(thread);
 		t.start();
 	}
     
-    private class HttpThread implements Runnable {
+  	//---------- CLASS HTTPTHREAD---------------------------------------------------------//
+    private class HttpThread implements Runnable 
+    {
+    	
+    	//-----Attributes---------------------------------------//
     	MainActivity parent;
 		private String sUrl;
-    	public HttpThread(MainActivity parent,String sUrl) {
+		
+		//-----Constructor--------------------------------------//
+    	public HttpThread(MainActivity parent,String sUrl) 
+    	{
 			this.parent = parent;
 			this.sUrl=sUrl;
-		}
+    	}
     	
-	    public void run() {
-	    	//HttpData ret = new HttpData();
+    	//----- RUN ---------------------------------------------//
+	    public void run() 
+		{    	
 			String str;
 			StringBuffer buff = new StringBuffer();
-			try {
+			try 
+			{	
 				URL url = new URL(this.sUrl);
-	 
 				BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-				while ((str = in.readLine()) != null) {
+				//typical appli : BufferedReader buf = new BufferedReader(new FileReader("file.java"));
+				//InputStreamReader(InputStream in)
+				
+				while ((str = in.readLine()) != null) 
+				{
 					buff.append(str);
 				}
-				//ret.content = buff.toString();
 			} catch (Exception e) {
-				Log.e("HttpRequest", e.toString());
+					Log.e("HttpRequest", e.toString());
 			}
-			// call display message activity
-			// using our response
+			
+			// call display message activity using our response
 			String response = buff.toString();
 			Intent intent = new Intent(parent, DisplayMessageActivity.class);
 			intent.putExtra(EXTRA_MESSAGE, response);
-	    	//putExtra()==takes a string as the key and the value in the second parameter.
-	    	startActivity(intent);
+		    //putExtra()==takes a string as the key and the value in the second parameter.
+		    startActivity(intent);
 	    }
     }
 }
