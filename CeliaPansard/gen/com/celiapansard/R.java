9b5fb866b90d0a2709337cbe3dea064daf63afa5
celia pansard <celia@celia-Precision-WorkStation-T3400.(none)>
2012-08-16 18:25:04 -0400

    ok

--- CeliaPansard/gen/com/celiapansard/R.java
+++ CeliaPansard/gen/com/celiapansard/R.java
@@ -19,7 +19,8 @@ public final class R {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int ic_midas=0x7f020002;
-        public static final int midaslogo=0x7f020003;
+        public static final int kitware_logo=0x7f020003;
+        public static final int midaslogo=0x7f020004;
     }
     public static final class id {
         public static final int ButtonGo=0x7f080010;
