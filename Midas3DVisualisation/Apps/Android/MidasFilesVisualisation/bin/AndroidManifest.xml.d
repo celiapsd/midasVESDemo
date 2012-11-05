323ed30ae04074aa4ec88fca98d7f27eee2234c7
Celia Pansard <celia.pansard@kitware.com>
2012-11-05 17:17:45 -0500

    CMakeFiles deleted

--- Midas3DVisualisation/Apps/Android/MidasFilesVisualisation/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/celia/midasVESDemo/Midas3DVisualisation/Apps/Android/MidasFilesVisualisation/bin/AndroidManifest.xml : \
-/home/celia/midasVESDemo/Midas3DVisualisation/Apps/Android/MidasFilesVisualisation/AndroidManifest.xml \
--- Midas3DVisualisation/Apps/Android/MidasFilesVisualisation/bin/MidasFilesVisualisation-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/home/celia/midasVESDemo/Midas3DVisualisation/Apps/Android/MidasFilesVisualisation/bin/MidasFilesVisualisation-debug-unaligned.apk : \
-/home/celia/midasVESDemo/Midas3DVisualisation/Apps/Android/MidasFilesVisualisation/bin/MidasFilesVisualisation.ap_ \
-/home/celia/midasVESDemo/Midas3DVisualisation/Apps/Android/MidasFilesVisualisation/bin/classes.dex \
-/home/celia/android-sdks/tools/support/annotations.jar \
-/home/celia/midasVESDemo/Midas3DVisualisation/Apps/Android/MidasFilesVisualisation/libs/armeabi-v7a/libKiwiNative.so \
-/home/celia/midasVESDemo/Midas3DVisualisation/Apps/Android/MidasFilesVisualisation/libs/armeabi-v7a/libMidasNative.so \
